5f83f4033b2cddc02453547a82e51a3fd31628ee
itallolobo <48370772+itallolobo@users.noreply.github.com>
2020-11-09 19:58:42 -0300

    Create arte.py

---
+++ arte.py
@@ -0,0 +1,16 @@
+from time import sleep
+def Logo(tempo,distancia):
+    print('')
+
+    print('_____________________________________________________'.center(distancia))
+    print('|  _____        _         _  _           _           |'.center(distancia))
+    print('| /  __ \      (_)       (_)| |         | |          |'.center(distancia))
+    print('| | /  \/ _ __  _  _ __   _ | |_   __ _ | |_   ___   |'.center(distancia))
+    print("| | |    | '__||'|| '_ \ | || __| / _` || __| / _ \  |".center(distancia))
+    print('| | \__/\| |   | || |_) || || |_ | (_| || |_ | (_) | |'.center(distancia))
+    print('|  \____/|_|   |_|| .__/ |_| \__| \__,_| \__| \___/  |'.center(distancia))
+    print('|                 | |                                |'.center(distancia))
+    print('|                 |_| by: lucas Gusmão e Itallo Lobo |'.center(distancia))
+    print('|____________________________________________________|'.center(distancia))
+    sleep(3)
+
